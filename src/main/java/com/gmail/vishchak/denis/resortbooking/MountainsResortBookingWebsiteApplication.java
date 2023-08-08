e0f48cffd97188bb8056aba323955589a5909e00
vishchak <denis.vishchak@gmail.com>
2023-08-09 09:41:14 +1000

    init commit;

---
+++ src/main/java/com/gmail/vishchak/denis/resortbooking/MountainsResortBookingWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.vishchak.denis.resortbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainsResortBookingWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MountainsResortBookingWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmail/vishchak/denis/resortbooking/MountainsResortBookingWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.vishchak.denis.resortbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountainsResortBookingWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
