92f190a501795d64f3176236a82709eb8f86ab03
vishchak <denis.vishchak@gmail.com>
2023-08-30 12:23:56 +1000

    Implement unit tests for AccommodationService methods
    - Added unit tests for getAllAccommodations method
    - Added unit tests for getAccommodationById method
    - Added unit tests for searchAccommodationsByCriteria method

---
+++ src/test/java/com/gmail/vishchak/denis/resortbooking/servicetest/AccommodationServiceTest.java
@@ -0,0 +1,92 @@
+package com.gmail.vishchak.denis.resortbooking.servicetest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import com.gmail.vishchak.denis.resortbooking.exception.custom.NoContentException;
+import com.gmail.vishchak.denis.resortbooking.model.Amenity;
+import com.gmail.vishchak.denis.resortbooking.model.Tag;
+import com.gmail.vishchak.denis.resortbooking.service.AccommodationService;
+import com.gmail.vishchak.denis.resortbooking.serviceimpl.AccommodationServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import com.gmail.vishchak.denis.resortbooking.model.Accommodation;
+import com.gmail.vishchak.denis.resortbooking.model.search.SearchCriteria;
+import com.gmail.vishchak.denis.resortbooking.repository.AccommodationRepository;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public class AccommodationServiceTest {
+    @Test
+    public void testGetAllAccommodations() {
+        AccommodationRepository repository = mock(AccommodationRepository.class);
+        AccommodationService service = new AccommodationServiceImpl(repository);
+
+        Page<Accommodation> mockPage = new PageImpl<>(Collections.emptyList());
+        Pageable pageable = Pageable.unpaged();
+        when(repository.findAll(pageable)).thenReturn(mockPage);
+
+        assertThrows(NoContentException.class, () -> service.getAllAccommodations(pageable));
+    }
+
+    @Test
+    public void testGetAccommodationById() {
+        AccommodationRepository repository = mock(AccommodationRepository.class);
+        AccommodationService service = new AccommodationServiceImpl(repository);
+
+        Accommodation mockAccommodation = Accommodation.builder()
+                .name("Sample Accommodation")
+                .description("This is a sample accommodation for testing purposes.")
+                .price(100.0)
+                .build();
+        when(repository.findById(1L)).thenReturn(Optional.of(mockAccommodation));
+
+        Accommodation result = service.getAccommodationById(1L);
+
+        assertNotNull(result);
+        assertEquals("Sample Accommodation", result.getName());
+    }
+
+    @Test
+    public void testSearchAccommodationsByCriteria() {
+        AccommodationRepository repository = mock(AccommodationRepository.class);
+        AccommodationService service = new AccommodationServiceImpl(repository);
+
+        SearchCriteria criteria = SearchCriteria.builder()
+                .keyword("Sample")
+                .amenities(Arrays.asList("Swimming Pool", "Wi-Fi"))
+                .tags(Arrays.asList("Beachfront", "Luxury"))
+                .build();
+
+        Pageable pageable = Pageable.unpaged();
+
+        List<Accommodation> mockAccommodations = Collections.singletonList(
+                Accommodation.builder()
+                        .name("Sample Accommodation")
+                        .description("This is a sample accommodation for testing purposes.")
+                        .price(100.0)
+                        .amenities(Collections.singletonList(
+                                Amenity.builder().name("Swimming Pool").build()
+                        ))
+                        .tags(Collections.singletonList(
+                                Tag.builder().tagName("Beachfront").build()
+                        ))
+                        .build()
+        );
+        Page<Accommodation> mockPage = new PageImpl<>(mockAccommodations);
+        when(repository.findAll(any(Specification.class), eq(pageable))).thenReturn(mockPage);
+
+        Page<Accommodation> result = service.searchAccommodationsByCriteria(criteria, pageable);
+
+        assertNotNull(result);
+        assertEquals(1, result.getTotalElements());
+    }
+
+}
+
