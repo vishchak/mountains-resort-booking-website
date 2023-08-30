7f8dc8cac3a62ff8d442d7c5374391ef6c316955
Denis <denis.vishchak@gmail.com>
2023-08-30 10:57:15 +1000

    Implement custom exceptions, record API errors, and create custom exception handler
    - Define custom exception classes for common error scenarios
    - Enhance service layer to throw custom exceptions with specific error messages
    - Implement a global exception handler to capture and handle exceptions
    - Use ResponseEntityExceptionHandler to customize error responses
    - Record API errors in a centralized error logging mechanism
    - Update documentation to include error responses for different scenarios

---
+++ src/main/java/com/gmail/vishchak/denis/resortbooking/exception/custom/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.gmail.vishchak.denis.resortbooking.exception.custom;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
+
